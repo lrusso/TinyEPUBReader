7a66c8034d213e9c46a600c2c2da69148359b4ed
Leonardo Russo <info@lrusso.com>
2020-06-07 19:37:11 -0300

    Add files via upload

--- source/reader.js
+++ source/reader.js
@@ -29,7 +29,7 @@ function setCookie(name,value,days) {
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    document.cookie = name + "=" + (value || "")  + expires + "; Secure; path=/";
 }
 
 function getCookie(name) {
